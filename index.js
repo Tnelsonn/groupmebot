c6a53036b59f46250b4822c117317cd087e02bdd
topher <47952908+Tnelsonn@users.noreply.github.com>
2022-11-02 19:58:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,15 +31,3 @@ function ping() {
   this.res.end("Wooglin has Arrived");
 }
 
-const {readFileSync, promises: fsPromises} = require('fs');
-
-
-function syncReadFile(filename) {
-  const contents = readFileSync(filename, 'utf-8');
-
-  const arr = contents.split(/\r?\n/);
-
-  console.log(arr); 
-
-  return arr;
-}
